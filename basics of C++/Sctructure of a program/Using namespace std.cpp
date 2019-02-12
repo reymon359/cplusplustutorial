3594e22ddca24974f706f60f7733133cfda492e3
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-02-12 16:21:20 +0100

    structure of a program ended

---
+++ basics of C++/Sctructure of a program/Using namespace std.cpp
@@ -0,0 +1,12 @@
+// my second program in C++
+#include <iostream>
+using namespace std;
+/* The above declaration allows all elements in the std namespace to be
+accessed in an unqualified manner (without the std:: prefix).
+With this in mind, the last example can be rewritten to make
+unqualified uses of cout as:*/
+int main ()
+{
+  cout << "Hello World! ";
+  cout << "I'm a C++ program";
+}
